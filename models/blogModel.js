08b94f37e93c746faaa84fcb5729d75542e8b375
Arnab Banerjee <arnabweb3.0@gmail.com>
2023-06-03 20:50:23 +0530

    Apply async/await in all existing endpoint functions

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  date: String,
-  content: String,
-  tags: [],
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: String,
+    author: String,
+    date: String,
+    content: String,
+    tags: [],
+  },
+  { versionKey: false }
+);
 
 const Blog = mongoose.model("Blog", blogSchema);
 
