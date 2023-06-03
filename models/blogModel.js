c2c7e5b9f965036ad56cbf400ceedb7691630007
Arnab Banerjee <arnabweb3.0@gmail.com>
2023-06-03 21:40:23 +0530

    Replace date property of blog with timestamps

--- models/blogModel.js
+++ models/blogModel.js
@@ -4,13 +4,12 @@ const blogSchema = new mongoose.Schema(
   {
     title: String,
     author: String,
-    date: String,
     content: String,
     tags: [],
   },
   { versionKey: false }
 );
-
+blogSchema.set("timestamps", true);
 const Blog = mongoose.model("Blog", blogSchema);
 
 export default Blog;
